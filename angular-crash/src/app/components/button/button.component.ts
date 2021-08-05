6cf47c8e8de92bf8d57c26a6f94e710a8536122c
Rubina Gill <rubina.gill@nagarro.com>
2021-08-05 10:36:49 +0400

    functionality to add new task

--- angular-crash/src/app/components/button/button.component.ts
+++ angular-crash/src/app/components/button/button.component.ts
@@ -1,23 +1,21 @@
-import { Component, OnInit,Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent implements OnInit {
-  @Input() text?:string;
-  @Input() color?:string;
+  @Input() text?: string;
+  @Input() color?: string;
 
   @Output() btnClick = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onClick() {
+    this.btnClick.emit();
   }
-
-  onClick(){
-this.btnClick.emit();
-  }
-
 }
--- angular-crash/src/app/components/header/header.component.ts
+++ angular-crash/src/app/components/header/header.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/services/ui.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  title :string = 'Task Tracker';
-  constructor() { }
+  title: string = 'Task Tracker';
+  showAddTask!: boolean;
+  subscription!: Subscription;
 
-  ngOnInit(): void {
+  constructor(private uiService: UiService) {
+    this.uiService.onToggle().subscribe((value)=>this.showAddTask=value);
   }
 
-  toggleAddTask(){
-    console.log("toggle");
+  ngOnInit(): void {}
+
+  toggleAddTask() {
+    this.uiService.toggleAddTask();
   }
 }
